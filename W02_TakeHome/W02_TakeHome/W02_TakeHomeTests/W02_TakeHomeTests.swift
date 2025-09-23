f51dfccd25bbf67f402ad0b6792cca9ee82e8f4a
EvelynnKH <161397711+EvelynnKH@users.noreply.github.com>
2025-09-23 20:03:24 +0700

    Add files via upload

---
+++ W02_TakeHome/W02_TakeHome/W02_TakeHomeTests/W02_TakeHomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  W02_TakeHomeTests.swift
+//  W02_TakeHomeTests
+//
+//  Created by student on 22/09/25.
+//
+
+import Testing
+@testable import W02_TakeHome
+
+struct W02_TakeHomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
